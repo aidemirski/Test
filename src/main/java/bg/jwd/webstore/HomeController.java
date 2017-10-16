9eded3d1b7e7405a5ad6f1fc63d03834ae83b814
ang <ang@ang-PC>
2017-10-16 17:36:18 +0300

    sfksdhf

--- src/main/java/bg/jwd/webstore/HomeController.java
+++ src/main/java/bg/jwd/webstore/HomeController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import bg.jwd.webstore.constants.UrlConstants;
-
+/*hgfhfhfh*/
 /**
  * Handles requests for the application home page.
  */
